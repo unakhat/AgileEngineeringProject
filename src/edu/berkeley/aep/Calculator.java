90208b9d145e8686c43856f081b32ecb4c33fc05
unakhat <unakhat@gmail.com>
2018-04-19 01:02:41 -0700

    Test class

---
+++ src/edu/berkeley/aep/Calculator.java
@@ -0,0 +1,43 @@
+package edu.berkeley.aep;
+
+public class Calculator {
+
+    public double a;
+    public double b;
+
+    public Calculator(double a, double b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    double add(double a, double b) {
+        double answer = a+b;
+        return answer;
+    }
+
+    double subtract() {
+        double answer = a-b;
+        return answer;
+    }
+
+    double multiply(double a, double b) {
+        double answer = a*b;
+        return answer;
+    }
+
+    double divide(double a, double b) {
+        double answer = a/b;
+        return answer;
+    }
+
+    double power(double a, double b){
+        double answer =a;
+
+        for (int x=2; x<=b; x++){
+            answer *= a;
+        }
+
+        return answer;
+    }
+
+}
