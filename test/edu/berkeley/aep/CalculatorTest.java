946ac73526e8f555afaede5cdd58a3866069d067
Udit Nakhat <unakhat@gmail.com>
2018-04-22 14:53:02 -0700

    Update CalculatorTest.java

--- test/edu/berkeley/aep/CalculatorTest.java
+++ test/edu/berkeley/aep/CalculatorTest.java
@@ -10,8 +10,18 @@ import java.util.*;
 public class CalculatorTest {
 
 
+  
     @Test
-    public void add()
+    public void addfourplusequalseven()
+    {
+        Calculator x=new Calculator(4.0,3.0);
+        TestCase.assertEquals(7.0,x.add());
+
+    }
+
+
+    @Test
+    public void subfourminusthreeequalone()
     {
         Calculator x=new Calculator(4.0,3.0);
         TestCase.assertEquals(7.0,x.add());
