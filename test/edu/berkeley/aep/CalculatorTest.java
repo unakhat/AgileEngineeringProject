68f2351135a67d04c479dd9102b30dda3f1b4ee1
Udit Nakhat <unakhat@gmail.com>
2018-04-22 14:55:21 -0700

    Update CalculatorTest.java

--- test/edu/berkeley/aep/CalculatorTest.java
+++ test/edu/berkeley/aep/CalculatorTest.java
@@ -27,6 +27,14 @@ public class CalculatorTest {
         TestCase.assertEquals(7.0,x.add());
 
     }
+  
+    @Test
+    public void multiplythreeandtwosix()
+    {
+        Calculator x=new Calculator(3.0,2.0);
+        TestCase.assertEquals(6.0,x.multiply());
+
+    }
 
 
 
